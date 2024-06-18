adb422aa0e245cf8ebe711036211a1efdc31969d
Laís Brum Menezes <laisbrme@gmail.com>
2024-06-17 23:20:00 -0300

    atualização nos estilos
    cores, fontes e métricas

---
+++ src/constants/fontes.js
@@ -0,0 +1,29 @@
+export const FONTES = {
+	regular: "Poppins_400Regular",
+	semiBold: "Poppins_600SemiBold",
+	extraLight: "Poppins_200ExtraLight",
+	lightItalic: "Poppins_300Light_Italic",
+	extraLightItalic: "Poppins_200ExtraLight_Italic",
+};
+
+/* 
+Estilos disponíveis da família:
+    Poppins_100Thin,
+    Poppins_100Thin_Italic,
+    Poppins_200ExtraLight,
+    Poppins_200ExtraLight_Italic,
+    Poppins_300Light,
+    Poppins_300Light_Italic,
+    Poppins_400Regular,
+    Poppins_400Regular_Italic,
+    Poppins_500Medium,
+    Poppins_500Medium_Italic,
+    Poppins_600SemiBold,
+    Poppins_600SemiBold_Italic,
+    Poppins_700Bold,
+    Poppins_700Bold_Italic,
+    Poppins_800ExtraBold,
+    Poppins_800ExtraBold_Italic,
+    Poppins_900Black,
+    Poppins_900Black_Italic,
+*/
---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+import { CORES } from "../constants/cores";
+import { FONTES } from "../constants/font";
+import { metrics } from "./metrics";
+
+export const theme = {
+	CORES,
+	FONTES,
+	metrics,
+};
