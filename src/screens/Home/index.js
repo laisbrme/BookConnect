d52272c3ac96be4a0e97ca5e3aee61a8e0b48a33
RhaisaJusto <rhaisagomesctt@gmail.com>
2024-06-19 21:52:18 -0300

    update 1 header home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,36 +2,40 @@ import React from "react";
 import { Container, MeuText } from "../../styles/styles-geral";
 import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 import { Feather } from "@expo/vector-icons";
+import {} from "@fontsource/poppins"
 
-export default function Home({ navigation }) {
+
+export default function Home( { navigation }) {
 	return (
-		<Container>
-			<MeuText fontSize={25} cor={"blue"}>
-				Início - BookConnect
-			</MeuText>
-		</Container>
-	);
-}
-
-export function Header() {
-	return (
-		<View style={styles.container}>
-			<text>
-				<b> Olá Luiz,</b> como é bom <br>te ver aqui.</br>
-			</text>
-
-			<TouchableOpacity activeOpacity={0.9} style={styles.buttonUser}>
-				<Feather nome="user" size={27} color="FFF" />
-			</TouchableOpacity>
+	  <View style={styles.container}>
+		<View style={styles.content}>
+		<Text style={styles.username}>Olá Luiz,como é bom te ver aqui.</Text>
+  
+		<TouchableOpacity activeOpacity={0.9} style={styles.buttonUser}>
+		  <Feather name="user" size={27} color="#333061" />
+		</TouchableOpacity>
+	  
+		</View>
 		</View>
 	);
-}
-
-const styles = StyleSheet.create({
-	content: {
-		flexDirection: "row",
-		paddingStart: 16,
-		paddingEnd: 16,
-		paddingBottom: 44,
-	},
+  }
+  
+  
+  
+  
+  const styles = StyleSheet.create({
+    container: {
+      flexDirection: "row",
+      paddingStart: 16,
+      paddingEnd: 16,
+      paddingBottom: 44,
+    },
+    content: {
+      flex: 1,
+      alignItems: "center",
+	  flexDirection: 'row',
+    }
+  
 });
+  
+  
