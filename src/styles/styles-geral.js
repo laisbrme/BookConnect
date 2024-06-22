5390bd730fd02d79f0c6401129f45b6f4d1e1143
Laís Brum Menezes <laisbrme@gmail.com>
2024-06-22 15:33:21 -0300

    adição dos planos de fundo roxo

--- src/styles/styles-geral.js
+++ src/styles/styles-geral.js
@@ -9,9 +9,34 @@ const Container = styled.View`
 	justify-content: center;
 	padding: 20px;
 `;
+
+const BackgroudRoxo1 = styled.View`
+	position: absolute;
+	flex: 1;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 48.74%;
+	background: ${colors.COR_PRIMARIA};
+	border-bottom-left-radius: 56px;
+	border-bottom-right-radius: 56px;
+`;
+
+const BackgroudRoxo2 = styled.View`
+	position: absolute;
+	flex: 1;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 57.53%;
+	background: ${colors.COR_PRIMARIA};
+	border-bottom-left-radius: 16px;
+	border-bottom-right-radius: 16px;
+`;
+
 const MeuText = styled.Text`
 	font-size: ${(props) => props.fontSize || 14};
 	color: ${(props) => props.cor || colors.TEXTO_ESCURO};
 `;
 
-export { Container, MeuText };
+export { Container, BackgroudRoxo1, BackgroudRoxo2, MeuText };
