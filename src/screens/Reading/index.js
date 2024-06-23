5a2f5788d6201da002a29288ab444cc8582a8321
Laís Brum Menezes <laisbrme@gmail.com>
2024-06-23 00:16:28 -0300

    Update Reading

--- src/screens/Reading/index.js
+++ src/screens/Reading/index.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { Container, BackgroudRoxo1, MeuText } from "../../styles/styles-geral";
+import { Container, BackgroundRoxo1, MeuText } from "../../styles/styles-geral";
 
 export default function Reading({ navigation }) {
 	return (
-		<BackgroudRoxo1>
+		<BackgroundRoxo1>
 			<Container>
-				<MeuText fontSize={25} cor={"blue"}>
-					Página Leituras
-				</MeuText>
+				<MeuText cor={"blue"}>Página Leituras</MeuText>
 			</Container>
-		</BackgroudRoxo1>
+		</BackgroundRoxo1>
 	);
 }
