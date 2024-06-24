f9662eb9ea6c7a662bf3242f9a98eaba82523b4b
Laís Brum Menezes <laisbrme@gmail.com>
2024-06-23 22:27:27 -0300

    Update styles da Home

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,53 +1,29 @@
-
 import styled from "styled-components/native";
-import { Feather } from "@expo/vector-icons";
-import { StatusBar } from "react-native";
-
-const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 22 : 64;
+// import { Feather } from "@expo/vector-icons";
 
 /* HEADER */
-const ContainerHome = styled.View ` 
-    flex-direction: column;
-    padding-top: 36px;
-    padding-right: 16px;
-    paddingBottom: 44px;
-    marginTop: 25px;
-    
-  
-    padding-Top: statusBarHeight;
-    background-Color: #333061;
-    height: 75%;
-    width: 100%;
-    border-end-start-radius: 50px;
-    border-end-end-radius: 50px;
-  `
 
-    
- 
- const Content = styled.View ` 
-    flex: 1;
-    align-items: flex-start;
-    flex-Direction: column; `
-    
-  
-const ButtonUser = styled.TouchableOpacity ` 
-    padding-Top: 25px;     
-    width: 44px;
-    height: 44px;
-    background-color: #F8F8F8;
-    justify-content: center;
-    align-items: center;
-    border-radius: 22px;;
-    padding: 10px;
-    `
-   
-  
-  
-  const Username = styled.Text ` 
-font-size: 18px;
-color: #FFF;
-font-weight: bold;
-`
+const Content = styled.View`
+	flex: 1;
+	align-items: flex-start;
+	flex-direction: column;
+`;
 
+const ButtonUser = styled.TouchableOpacity`
+	padding-top: 25px;
+	width: 44px;
+	height: 44px;
+	background-color: #f8f8f8;
+	justify-content: center;
+	align-items: center;
+	border-radius: 22px;
+	padding: 10px;
+`;
 
-export { ContainerHome, ButtonUser, Content, Username }
+const Username = styled.Text`
+	font-size: 18px;
+	color: #fff;
+	font-weight: bold;
+`;
+
+export { ButtonUser, Content, Username };
