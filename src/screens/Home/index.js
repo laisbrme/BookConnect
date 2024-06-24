94e8d864256a64486c5484a3b3646e1f16154626
Laís Brum Menezes <laisbrme@gmail.com>
2024-06-23 22:29:16 -0300

    Update tela Home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,34 +1,22 @@
 import React from "react";
-import { Container, MeuText } from "../../styles/styles-geral";
-import { View, StyleSheet, Text, TouchableOpacity, StatusBar } from "react-native";
+// import { Container, MeuText } from "../../styles/styles-geral";
+// import { View, StyleSheet, Text, TouchableOpacity, StatusBar } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import {} from "@fontsource/poppins";
-import {  ContainerHome, ButtonUser, Content, Username  } from "./styles.js";
+import { ButtonUser, Content, Username } from "./styles.js";
+import { Background1 } from "../../components/Backgrounds"
 
-
-const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 22 : 64;
-
+// const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 22 : 64;
 
 export default function Home({ navigation }) {
-  return (
-    <ContainerHome>
-      <Content>
-        <Username>
-        
-        Olá Luiz, como é bom te ver aqui.
-
-        </Username>
-
-        <ButtonUser>
-          <Feather name="user" size={27} color="#333061" />
-        </ButtonUser>
-      
-      </Content>
-    </ContainerHome>
-  );
+	return (
+		<Background1>
+			<Content>
+				<ButtonUser>
+					<Feather name="user" size={27} color="#333061" />
+				</ButtonUser>
+				<Username>Olá Luiz, como é bom te ver aqui.</Username>
+			</Content>
+		</Background1>
+	);
 }
-
-
-
-
-  
