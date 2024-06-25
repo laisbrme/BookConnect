839dcd0727d78f2b476e464e2e38257c808d2e61
Laís Brum Menezes <laisbrme@gmail.com>
2024-06-25 16:31:22 -0300

    Update tela reading

---
+++ src/screens/Reading/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components/native";
+import { theme } from "../../styles/theme";
+
+const LayoutText = styled.View`
+	display: flex;
+	width: ${theme.metrics.px(210)}px;
+	height: ${theme.metrics.px(45)}px;
+	margin-right: ${theme.metrics.px(145)}px;
+	flex-direction: column;
+	align-items: center;
+	flex-shrink: 0;
+	line-height: 109%;
+`;
+
+const ListCards = styled.View`
+	flex-wrap: wrap;
+`;
+
+export { LayoutText, ListCards };
