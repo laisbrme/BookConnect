6b82c2d06cc3e74e7ffb945a0016687dfc880f8f
Laís Brum Menezes <laisbrme@gmail.com>
2024-06-25 00:04:51 -0300

    Update SearchBar

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, BoxIconSearch, SearchIcon, Input, Text } from "./styles";
+import { Container, BoxIconSearch, SearchIcon, Input } from "./styles";
 
 export default function SearchBar(props) {
 	return (
