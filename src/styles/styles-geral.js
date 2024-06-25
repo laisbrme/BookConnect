7be092e08bd821dbf57dbdc54a70e07bcfafb821
Laís Brum Menezes <laisbrme@gmail.com>
2024-06-25 00:36:30 -0300

    Update styles-geral

--- src/styles/styles-geral.js
+++ src/styles/styles-geral.js
@@ -1,11 +1,10 @@
 import styled from "styled-components/native";
-import { colors } from "./colors";
-import { fonts } from "./fonts";
+import {theme } from "./theme";
 
 const Container = styled.View`
 	flex: 1;
 	background-color: ${(props) =>
-		props.selecionado ? colors.COR_PRIMARIA : colors.COR_FUNDO_LIGHT};
+		props.selecionado ? theme.colors.COR_PRIMARIA : theme.colors.COR_FUNDO_LIGHT};
 	align-items: center;
 	justify-content: center;
 	padding-left: 20px;
@@ -13,9 +12,9 @@ const Container = styled.View`
 `;
 
 const MeuText = styled.Text`
-	font-size: ${(props) => props.fontSize || 18}px;
-	color: ${(props) => props.cor || colors.TEXTO_ESCURO};
-	font-family: ${(props) => props.fontFamily || fonts.regular};
+	font-size: ${(props) => props.fontSize || theme.metrics.px(18)}px;
+	color: ${(props) => props.cor || theme.colors.TEXTO_ESCURO};
+	font-family: ${(props) => props.fontFamily || theme.fonts.regular};
 	line-height: 109%;
 `;
 
