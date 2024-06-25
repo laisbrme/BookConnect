2fac843f48584a4a93fdbaf123724dbc63226b72
Laís Brum Menezes <laisbrme@gmail.com>
2024-06-25 11:22:36 -0300

    Update SearchBar

--- src/components/SearchBar/styles.js
+++ src/components/SearchBar/styles.js
@@ -1,44 +1,41 @@
 import styled from "styled-components/native";
 import { TextInput } from "react-native";
-import { metrics } from "../../styles/metrics";
-import { colors } from "../../styles/colors";
-import { fonts } from "../../styles/fonts";
 import Icon from "react-native-vector-icons/MaterialIcons";
+import { theme } from "../../styles/theme";
 
 export const Container = styled.SafeAreaView`
-	width: ${metrics.px(356)}px;
-	height: ${metrics.px(38)}px;
+	width: ${theme.metrics.px(356)}px;
+	height: ${theme.metrics.px(38)}px;
 	flex-shrink: 0;
-	background: ${colors.BRANCO};
-	border-radius: ${metrics.px(8)}px;
+	background: ${theme.colors.BRANCO};
+	border-radius: ${theme.metrics.px(8)}px;
 `;
 
 export const Input = styled(TextInput).attrs({
 	placeholder: "pesquisar...",
 })`
 	flex: 1;
-	margin-left: ${metrics.px(15)}px;
-	margin-right: ${metrics.px(45)}px;
-	color: ${colors.CINZA};
-    font-family: ${fonts.lightItalic};
-
+	margin-left: ${theme.metrics.px(15)}px;
+	margin-right: ${theme.metrics.px(45)}px;
+	color: ${theme.colors.CINZA};
+	font-family: ${theme.fonts.lightItalic};
 `;
 
 export const BoxIconSearch = styled.View`
 	display: flex;
-	width: ${metrics.px(38)}px;
-	height: ${metrics.px(38)}px;
+	width: ${theme.metrics.px(38)}px;
+	height: ${theme.metrics.px(38)}px;
 	justify-content: center;
 	align-items: center;
 	flex-shrink: 0;
 	position: absolute;
 	right: 0px;
-	border-radius: ${metrics.px(8)}px;
+	border-radius: ${theme.metrics.px(8)}px;
 `;
 
 export const SearchIcon = styled(Icon).attrs({
 	name: "search",
 })`
-	color: ${colors.CINZA};
-	font-size: ${metrics.px(18)}px;
+	color: ${theme.colors.CINZA};
+	font-size: ${theme.metrics.px(18)}px;
 `;
