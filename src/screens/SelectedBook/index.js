eab29f07d3561c0732189787ba6043edd660b2b9
Laís Brum Menezes <laisbrme@gmail.com>
2024-06-29 17:37:39 -0300

    Criação da tela SelectedBook

---
+++ src/screens/SelectedBook/index.js
@@ -0,0 +1,334 @@
+import React from "react";
+import { ScrollView } from "react-native";
+import { MyButton } from "../../components/MyButtons";
+import {
+	MaskGroup,
+	BookCover,
+	InterationGroup,
+	TextBox,
+	InteractiveBox,
+	BoxIcons,
+	BoxIconsText,
+	BookmarkIcon,
+	InteractiveTextBox,
+	HeartIcon,
+	ChatIcon,
+	ButtonView,
+	ListChapters,
+	TitleChapter,
+	ChapterView,
+	TextChapter,
+	BoxCheckIcon,
+	CheckIcon,
+} from "./styles";
+import { MeuText } from "../../styles/styles-geral";
+import { theme } from "../../styles/theme";
+
+export default function SelectedBook({ navigation }) {
+	const Comentar = (props) => {
+		navigation.navigate("Comments");
+	};
+
+	return (
+		<ScrollView>
+			<MaskGroup>
+				<BookCover source={{ uri: "../../../assets/books/LayerBook.png" }} />
+			</MaskGroup>
+			<InterationGroup>
+				<TextBox>
+					<MeuText
+						fontSize={theme.metrics.px(14)}
+						cor={theme.colors.BRANCO}
+						fontFamily={theme.fonts.semiBold}
+					>
+						O Meu pé de Laranja Lima
+					</MeuText>
+					<MeuText
+						fontSize={theme.metrics.px(12)}
+						cor={theme.colors.BRANCO}
+						fontFamily={theme.fonts.regular}
+						mt={theme.metrics.px(10)}
+					>
+						José Vasconcelos
+					</MeuText>
+					<MeuText
+						fontSize={theme.metrics.px(12)}
+						cor={theme.colors.BRANCO}
+						fontFamily={theme.fonts.extraLight}
+						mt={theme.metrics.px(10)}
+					>
+						Este livro retrata a história de um menino de seis anos chamado
+						Zezé, que pertencia a uma família muito pobre e numerosa. Sua mãe
+						trabalhava numa fábrica e o pai estava desempregado. Passavam por
+						muitas dificuldades, pelo que as irmãs mais velhas tomavam conta dos
+						mais novos e, por sua vez, Zezé tomava conta do seu irmão mais novo,
+						Luís.
+					</MeuText>
+				</TextBox>
+				<InteractiveBox>
+					<BoxIconsText>
+						<BoxIcons>
+							<BookmarkIcon />
+						</BoxIcons>
+						<InteractiveTextBox>
+							<MeuText
+								fontSize={theme.metrics.px(11)}
+								cor={theme.colors.BRANCO}
+							>
+								Adicionar às Leituras
+							</MeuText>
+						</InteractiveTextBox>
+					</BoxIconsText>
+					<BoxIconsText>
+						<BoxIcons>
+							<HeartIcon />
+						</BoxIcons>
+						<InteractiveTextBox>
+							<MeuText
+								fontSize={theme.metrics.px(11)}
+								cor={theme.colors.BRANCO}
+							>
+								Adicionar aos Favoritos
+							</MeuText>
+						</InteractiveTextBox>
+					</BoxIconsText>
+					<BoxIconsText>
+						<BoxIcons>
+							<ChatIcon />
+						</BoxIcons>
+						<InteractiveTextBox>
+							<MeuText
+								fontSize={theme.metrics.px(11)}
+								cor={theme.colors.BRANCO}
+							>
+								Comentar Livro
+							</MeuText>
+						</InteractiveTextBox>
+					</BoxIconsText>
+				</InteractiveBox>
+			</InterationGroup>
+			<ButtonView>
+				<MeuText
+					fontSize={theme.metrics.px(12)}
+					cor={theme.colors.BRANCO}
+					onPress={Comentar("Reescreva o final do livro O Meu Pé de Laranja Lima")}
+				>
+					Reescreva seu Final
+				</MeuText>
+			</ButtonView>
+			<ListChapters>
+				<TitleChapter>
+					<MeuText
+						fontSize={theme.metrics.px(14)}
+						fontFamily={theme.fonts.semiBold}
+					>
+						CAPÍTULOS
+					</MeuText>
+				</TitleChapter>
+				<TitleChapter>
+					<MeuText fontSize={theme.metrics.px(12)}>PRIMEIRA PARTE</MeuText>
+				</TitleChapter>
+
+				<ChapterView>
+					<TextChapter>
+						<MeuText
+							fontSize={theme.metrics.px(12)}
+							fontFamily={theme.fonts.light}
+						>
+							O descobridor das coisas
+						</MeuText>
+					</TextChapter>
+					<BoxCheckIcon>
+						<CheckIcon />
+					</BoxCheckIcon>
+				</ChapterView>
+
+				<ChapterView>
+					<TextChapter>
+						<MeuText
+							fontSize={theme.metrics.px(12)}
+							fontFamily={theme.fonts.light}
+						>
+							Um certo pé de laranja lima
+						</MeuText>
+					</TextChapter>
+					<BoxCheckIcon>
+						<CheckIcon />
+					</BoxCheckIcon>
+				</ChapterView>
+
+				<ChapterView>
+					<TextChapter>
+						<MeuText
+							fontSize={theme.metrics.px(12)}
+							fontFamily={theme.fonts.light}
+						>
+							Os dedos magros da pobreza
+						</MeuText>
+					</TextChapter>
+					<BoxCheckIcon>
+						<CheckIcon />
+					</BoxCheckIcon>
+				</ChapterView>
+
+				<ChapterView>
+					<TextChapter>
+						<MeuText
+							fontSize={theme.metrics.px(12)}
+							fontFamily={theme.fonts.light}
+						>
+							O passarinho, a escola e a flor
+						</MeuText>
+					</TextChapter>
+					<BoxCheckIcon>
+						<CheckIcon />
+					</BoxCheckIcon>
+				</ChapterView>
+
+				<ChapterView>
+					<TextChapter>
+						<MeuText
+							fontSize={theme.metrics.px(12)}
+							fontFamily={theme.fonts.light}
+						>
+							“Numa cadeia eu hei de verte-te morrer”
+						</MeuText>
+					</TextChapter>
+					<BoxCheckIcon>
+						<CheckIcon />
+					</BoxCheckIcon>
+				</ChapterView>
+
+				<TitleChapter>
+					<MeuText fontSize={theme.metrics.px(12)}>SEGUNDA PARTE</MeuText>
+				</TitleChapter>
+
+				<ChapterView>
+					<TextChapter>
+						<MeuText
+							fontSize={theme.metrics.px(12)}
+							fontFamily={theme.fonts.light}
+						>
+							O morcego
+						</MeuText>
+					</TextChapter>
+					<BoxCheckIcon>
+						<CheckIcon />
+					</BoxCheckIcon>
+				</ChapterView>
+
+				<ChapterView>
+					<TextChapter>
+						<MeuText
+							fontSize={theme.metrics.px(12)}
+							fontFamily={theme.fonts.light}
+						>
+							A conquista
+						</MeuText>
+					</TextChapter>
+					<BoxCheckIcon>
+						<CheckIcon />
+					</BoxCheckIcon>
+				</ChapterView>
+
+				<ChapterView>
+					<TextChapter>
+						<MeuText
+							fontSize={theme.metrics.px(12)}
+							fontFamily={theme.fonts.light}
+						>
+							Conversa para lá e para cá
+						</MeuText>
+					</TextChapter>
+					<BoxCheckIcon>
+						<CheckIcon />
+					</BoxCheckIcon>
+				</ChapterView>
+
+				<ChapterView>
+					<TextChapter>
+						<MeuText
+							fontSize={theme.metrics.px(12)}
+							fontFamily={theme.fonts.light}
+						>
+							Duas surras memoráveis
+						</MeuText>
+					</TextChapter>
+					<BoxCheckIcon>
+						<CheckIcon />
+					</BoxCheckIcon>
+				</ChapterView>
+
+				<ChapterView>
+					<TextChapter>
+						<MeuText
+							fontSize={theme.metrics.px(12)}
+							fontFamily={theme.fonts.light}
+						>
+							Suave e estranho pedido
+						</MeuText>
+					</TextChapter>
+					<BoxCheckIcon>
+						<CheckIcon />
+					</BoxCheckIcon>
+				</ChapterView>
+
+				<ChapterView>
+					<TextChapter>
+						<MeuText
+							fontSize={theme.metrics.px(12)}
+							fontFamily={theme.fonts.light}
+						>
+							De pedaço em pedaço é que se faz ternura
+						</MeuText>
+					</TextChapter>
+					<BoxCheckIcon>
+						<CheckIcon />
+					</BoxCheckIcon>
+				</ChapterView>
+
+				<ChapterView>
+					<TextChapter>
+						<MeuText
+							fontSize={theme.metrics.px(12)}
+							fontFamily={theme.fonts.light}
+						>
+							O Mangaratiba
+						</MeuText>
+					</TextChapter>
+					<BoxCheckIcon>
+						<CheckIcon />
+					</BoxCheckIcon>
+				</ChapterView>
+
+				<ChapterView>
+					<TextChapter>
+						<MeuText
+							fontSize={theme.metrics.px(12)}
+							fontFamily={theme.fonts.light}
+						>
+							Tantas são as velhas árvores
+						</MeuText>
+					</TextChapter>
+					<BoxCheckIcon>
+						<CheckIcon />
+					</BoxCheckIcon>
+				</ChapterView>
+
+				<ChapterView>
+					<TextChapter>
+						<MeuText
+							fontSize={theme.metrics.px(12)}
+							fontFamily={theme.fonts.light}
+						>
+							A confissão final
+						</MeuText>
+					</TextChapter>
+					<BoxCheckIcon>
+						<CheckIcon />
+					</BoxCheckIcon>
+				</ChapterView>
+			</ListChapters>
+		</ScrollView>
+	);
+}
---
+++ src/screens/SelectedBook/styles.js
@@ -0,0 +1,170 @@
+import styled from "styled-components/native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import IconChat from "react-native-vector-icons/MaterialIcons";
+import { theme } from "../../styles/theme";
+import { Button } from "react-native";
+
+export const MaskGroup = styled.View`
+	display: flex;
+	width: 100%;
+	height: ${theme.metrics.px(287)}px;
+`;
+
+export const BookCover = styled.ImageBackground`
+	width: 100%;
+	height: 100%;
+`;
+
+export const InterationGroup = styled.View`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	align-items: center;
+	width: 100%;
+	height: ${theme.metrics.px(263)}px;
+	background-color: ${theme.colors.ROXO};
+	border-bottom-left-radius: ${theme.metrics.px(16)}px;
+	border-bottom-right-radius: ${theme.metrics.px(16)}px;
+`;
+
+export const TextBox = styled.View`
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	width: ${theme.metrics.px(349)}px;
+	height: ${theme.metrics.px(159)}px;
+	line-height: 160%;
+	margin-top: ${theme.metrics.px(33)}px;
+`;
+
+export const InteractiveBox = styled.View`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	width: ${theme.metrics.px(349)}px;
+	height: ${theme.metrics.px(24)}px;
+	margin-bottom: ${theme.metrics.px(33)}px;
+`;
+
+export const BoxIconsText = styled.TouchableOpacity`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	width: ${theme.metrics.px(110)}px;
+	height: ${theme.metrics.px(24)}px;
+`;
+
+export const InteractiveTextBox = styled.Text`
+	display: flex;
+	width: 69px;
+	height: 100%;
+	justify-content: center;
+	align-items: center;
+	flex-shrink: 0;
+	position: absolute;
+	right: 0px;
+    line-height: 90%;
+`;
+
+export const BoxIcons = styled.View`
+	display: flex;
+	width: ${theme.metrics.px(24)}px;
+	height: ${theme.metrics.px(24)}px;
+	justify-content: center;
+	align-items: center;
+	flex-shrink: 0;
+	position: absolute;
+	left: 0px;
+`;
+
+export const BookmarkIcon = styled(Icon).attrs({
+	name: "bookmark-outline",
+})`
+	color: ${theme.colors.BRANCO};
+	font-size: ${theme.metrics.px(22)}px;
+`;
+
+export const HeartIcon = styled(Icon).attrs({
+	name: "cards-heart-outline",
+})`
+	color: ${theme.colors.BRANCO};
+	font-size: ${theme.metrics.px(22)}px;
+`;
+
+export const ChatIcon = styled(IconChat).attrs({
+	name: "chat-bubble-outline",
+})`
+	color: ${theme.colors.BRANCO};
+	font-size: ${theme.metrics.px(22)}px;
+`;
+
+export const ButtonView = styled.TouchableOpacity`
+    display: flex;
+    width: ${theme.metrics.px(343)}px;
+    height: ${theme.metrics.px(42)}px;
+    margin-top: ${theme.metrics.px(24)}px;
+    margin-left: ${theme.metrics.px(43)}px;
+    border-radius: ${theme.metrics.px(4)}px;
+    justify-content: center;
+    align-items: center;
+    flex-shrink: 0;
+    background-color: ${theme.colors.ROXO};
+`;
+
+export const ListChapters = styled.View`
+    display: flex;
+	flex-direction: column;
+    width: ${theme.metrics.px(343)}px;
+    height: auto;
+    margin-top: ${theme.metrics.px(14)}px;
+    margin-left: ${theme.metrics.px(43)}px;
+    flex-shrink: 0;
+`;
+
+export const TitleChapter = styled.View`
+	display: flex;
+    justify-content: space-between;
+	width: 100%;
+	height: auto;
+    padding-top: ${theme.metrics.px(10)}px;
+    padding-bottom: ${theme.metrics.px(5)}px;
+`;
+
+export const ChapterView = styled.View`
+    display: flex;
+    width: 100%;
+    height: ${theme.metrics.px(42)}px;
+    margin-top: ${theme.metrics.px(5)}px;
+    padding-left: ${theme.metrics.px(14)}px;
+    padding: ${theme.metrics.px(14)}px;
+    border-radius: ${theme.metrics.px(4)}px;
+    border: ${theme.metrics.px(0.5)}px solid ${theme.colors.CINZA};
+    justify-content: space-around;
+    flex-shrink: 0;
+    background-color: lightgray;
+`;
+
+export const TextChapter = styled.View`
+	display: flex;
+    justify-content: flex-start;
+	width: ${theme.metrics.px(291)}px;
+	height: ${theme.metrics.px(20)}px;
+`;
+
+export const CheckIcon = styled(IconChat).attrs({
+	name: "check-circle-outline",
+})`
+	color: ${theme.colors.ROXO};
+	font-size: ${theme.metrics.px(24)}px;
+`;
+
+export const BoxCheckIcon = styled.TouchableOpacity`
+	display: flex;
+	width: ${theme.metrics.px(42)}px;
+	height: 100%;
+	justify-content: center;
+	align-items: center;
+	flex-shrink: 0;
+	position: absolute;
+	right: 0px;
+`;
